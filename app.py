736d2af64f8508c7010cf65e7db9695b4264e0fd
Rohit Mahajan <147400563+rohitmahajanbootnext@users.noreply.github.com>
2026-03-13 19:46:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "I am Palak!!!!"
 
+@app.route('/sales/all')
+def salesAll():
+    return '{"month":"Sep","amount":1000000}'
+
 if __name__ == '__main__':
     app.run(debug=True, port=10000)
